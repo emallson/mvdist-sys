82244a2161227f3482e42e7a26b860c2400bc442
J David Smith <emallson@atlanis.net>
2017-01-17 15:32:34 -0500

    Build order matters. mvdist.f90 first

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate gcc;
 
 fn main() {
     gcc::compile_library("libmvdist.a",
-                         &["src/fortran/mvwrap.f90", "src/fortran/mvdist.f90"]);
+                         &["src/fortran/mvdist.f90", "src/fortran/mvwrap.f90"]);
     println!("cargo:rustc-link-lib=dylib=gfortran");
     println!("cargo:rustc-link-lib=dylib=gfortranbegin");
 }
